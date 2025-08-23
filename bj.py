a3eec9a640dc1f1cdb34d0abf31df03e6c240c56
st1vms <96783398+st1vms@users.noreply.github.com>
2025-08-23 13:16:54 +0200

    Hi-Lo stats

--- bj.py
+++ bj.py
@@ -7,7 +7,7 @@ BASIC_STRATEGY_CSV_PATH = "BasicStrategy.csv"
 BASIC_STRATEGY_DATAFRAME = read_csv(BASIC_STRATEGY_CSV_PATH, sep=";", header=None)
 
 
-def basic_strategy(player_hand: list[int], dealer_card: int, is_pair: bool):
+def basic_strategy(player_hand: list[int], dealer_card: int, is_pair: bool) -> str:
     """Returns an action (H=Hit, S=Stand, P=Split, Dh/Ds=Double Down)"""
 
     if dealer_card == 0:
@@ -71,8 +71,7 @@ class BJSimulation:
         seat_index: int = -1
         is_dealer: bool = False
         prev_cards_left: int = 0
-        prev_running_count: int = 0
-        prev_true_count: float = 0.0
+        prev_hilo_count: tuple[int, float] = (0, 0.0)
         # Count of the remaining cards by rank
         prev_rank_densities: dict[str, int] = {rank_id: 0 for rank_id in range(0, 13)}
         outcome_win: bool = False
@@ -100,7 +99,80 @@ class BJSimulation:
         # seat 7 is the dealer, seat 0 to 6 are players from right to left.
         self.players = [BJSimulation.Player(seat_index) for seat_index in range(0, 8)]
 
-    def calculate_round_stats(self) -> None: ...
+        self.prev_cards_left = len(self.deck)
+        self.prev_rank_densities = {rank_id: 0 for rank_id in range(0, 13)}
+        self.prev_hilo_count = (0, 0.0)  # Running count, True Count
+
+        self.current_rank_densities = {rank_id: 0 for rank_id in range(0, 13)}
+        self.current_hilo_count = (0, 0.0)  # Running count, True Count
+
+    def calculate_round_stats(self) -> None:
+        # Append previous round stats
+
+        dealer_score, is_soft = hand_score(self.players[-1].hands[0])
+        is_win = lambda hand: hand_score(hand)[0] > dealer_score
+
+        # Player seats stats
+        for seat_index in range(0, 7):
+            stats = BJSimulation.RoundSeatStatistics(
+                self.current_round_id,
+                seat_index,  # seat index
+                0,  # It's not the dealer
+                self.prev_cards_left,
+                self.prev_hilo_count,
+                self.prev_rank_densities,
+            )
+
+            # Calculate winning outcome for the seat
+            player_hands = self.players[seat_index]
+            if len(player_hands) > 1:
+                # Deal with split
+                stats.outcome_win = int(any([is_win(hand) for hand in player_hands]))
+            else:
+                stats.outcome_win = int(is_win(player_hands[0]))
+
+            self.round_stats.append(stats)
+
+        # Dealer's seat stats
+        self.round_stats.append(
+            BJSimulation.RoundSeatStatistics(
+                self.current_round_id,
+                seat_index,  # seat index
+                1,  # It's the dealer
+                self.prev_cards_left,
+                self.prev_hilo_count,
+                self.prev_rank_densities,
+                int(dealer_score <= 21),  # Dealers wins if it doesn't bust
+            )
+        )
+
+        # TODO Update previous-round stats
+        self.prev_cards_left = len(self.deck)
+        self.prev_hilo_count = (self.current_hilo_count[0], self.current_hilo_count[1])
+
+    def update_hi_lo_score(self, card: int):
+        """
+        Calculate the Hi-Lo running and true count based on the card log.
+        """
+        running = self.current_hilo_count[0]  # Take current running count
+
+        # Hi-Lo count system
+        if card == 0 or (9 <= card <= 12):
+            running -= 1  # High card
+        elif 1 <= card <= 5:
+            running += 1  # Low card
+
+        # Recalculate true count
+        decks_remaining = max(len(self.deck) / 52, 1 / 52)  # Avoid division by zero
+        true_count = running / decks_remaining
+
+        self.current_hilo_count = (running, round(true_count, 2))
+
+    def draw_card(self) -> int:
+        """Draw a card from the deck"""
+        card = self.deck.pop(0)
+        self.update_hi_lo_score(card)
+        return card
 
     def create_deck(self) -> list[int]:
         # 0 = A, 1 = 2, ... 12 = K
@@ -129,12 +201,12 @@ class BJSimulation:
         # Deal one card to each player two times in a row
         for _ in range(2):
             for seat_index in range(0, 8):
-                card = self.deck.pop(0)
+                card = self.draw_card()
                 self.players[seat_index].hands[0].append(card)
 
     def player_turn(
         self, seat_index: int, hand: list[int] = None, is_split: bool = False
-    ) -> bool:
+    ) -> None:
 
         player = self.players[seat_index]
 
@@ -162,20 +234,20 @@ class BJSimulation:
 
             if action == "H":
                 # Hit
-                hand.append(self.deck.pop(0))
+                hand.append(self.draw_card())
 
                 # Recalculate score
                 player_score, is_soft = hand_score(hand)
             elif action in {"Dh", "Ds"}:
                 # Double down
-                hand.append(self.deck.pop(0))  # deal exactly one card
+                hand.append(self.draw_card())  # deal exactly one card
                 player_score, is_soft = hand_score(hand)
                 return
             elif action == "P" and not is_split:
                 # Split hands
                 player.hands.remove(hand)
-                hand_right = [hand[0], self.deck.pop(0)]
-                hand_left = [hand[1], self.deck.pop(0)]
+                hand_right = [hand[0], self.draw_card()]
+                hand_left = [hand[1], self.draw_card()]
                 player.hands.extend([hand_right, hand_left])
 
                 self.player_turn(seat_index, hand=hand_right, is_split=True)
@@ -189,7 +261,7 @@ class BJSimulation:
 
         while score < 17:
             # Hit on 16
-            hand.append(self.deck.pop(0))
+            hand.append(self.draw_card())
             score, is_soft = hand_score(hand)
 
     def simulate_round(self) -> None:
